cd614b36dbc1326cc5de911f58beded217e9ae56
amineh.ahm <amineh.ahm@gmail.com>
2017-11-07 23:13:37 +0330

    added comments

--- syscall.c
+++ syscall.c
@@ -1,10 +1,13 @@
+/*
+ This code uses a system call to get the current time on the CPU then prints it as a date
+ Developed by Saba Zarbaaf
+ */
+
 #include <sys/time.h>
 #include<stdio.h>
 #include<time.h>
 
-
 int main(){
-
 
 struct timeval tval;
 gettimeofday(&tval,NULL);
@@ -12,6 +15,7 @@ gettimeofday(&tval,NULL);
 struct tm *timeinfo;
 timeinfo=localtime(&tval.tv_sec);
 printf("time is:%s",asctime(timeinfo));
+    
 return 0;
 }
 
